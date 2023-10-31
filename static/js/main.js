4c0d064ed7fc2e551b6b9ec240182ed43c3b4b6b
berluskuni <berluskuni@gmail.com>
2023-10-31 11:44:55 +0200

    fix readmore plugin_13

--- static/js/main.js
+++ static/js/main.js
@@ -298,7 +298,7 @@ $(".popular__item").hover(function () {
 });
 
 //phone mask
-$(".phone-mask").mask("+7(099)999-99-99");
+$(".phone-mask").mask("+7(999)999-99-99");
 
 //ajax
 function gratitudeOpen() {
